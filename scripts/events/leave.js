31b688b888e77aa8e267411b3a7d8aeed219eeee
Sumon islam <sskjibon1026@gmail.com>
2026-03-04 23:49:28 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -1,98 +1,59 @@
-const { getTime, drive } = global.utils;
-
-module.exports = {
-	config: {
-		name: "leave",
-		version: "1.4",
-		author: "NTKhang",
-		category: "events"
-	},
-
-	langs: {
-		vi: {
-			session1: "sáng",
-			session2: "trưa",
-			session3: "chiều",
-			session4: "tối",
-			leaveType1: "tự rời",
-			leaveType2: "bị kick",
-			defaultLeaveMessage: "{userName} đã {type} khỏi nhóm"
-		},
-		en: {
-			session1: "morning",
-			session2: "noon",
-			session3: "afternoon",
-			session4: "evening",
-			leaveType1: "left",
-			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
-		}
-	},
-
-	onStart: async ({ threadsData, message, event, api, usersData, getLang }) => {
-		if (event.logMessageType == "log:unsubscribe")
-			return async function () {
-				const { threadID } = event;
-				const threadData = await threadsData.get(threadID);
-				if (!threadData.settings.sendLeaveMessage)
-					return;
-				const { leftParticipantFbId } = event.logMessageData;
-				if (leftParticipantFbId == api.getCurrentUserID())
-					return;
-				const hours = getTime("HH");
-
-				const threadName = threadData.threadName;
-				const userName = await usersData.getName(leftParticipantFbId);
-
-				// {userName}   : name of the user who left the group
-				// {type}       : type of the message (leave)
-				// {boxName}    : name of the box
-				// {threadName} : name of the box
-				// {time}       : time
-				// {session}    : session
-
-				let { leaveMessage = getLang("defaultLeaveMessage") } = threadData.data;
-				const form = {
-					mentions: leaveMessage.match(/\{userNameTag\}/g) ? [{
-						tag: userName,
-						id: leftParticipantFbId
-					}] : null
-				};
-
-				leaveMessage = leaveMessage
-					.replace(/\{userName\}|\{userNameTag\}/g, userName)
-					.replace(/\{type\}/g, leftParticipantFbId == event.author ? getLang("leaveType1") : getLang("leaveType2"))
-					.replace(/\{threadName\}|\{boxName\}/g, threadName)
-					.replace(/\{time\}/g, hours)
-					.replace(/\{session\}/g, hours <= 10 ?
-						getLang("session1") :
-						hours <= 12 ?
-							getLang("session2") :
-							hours <= 18 ?
-								getLang("session3") :
-								getLang("session4")
-					);
-
-				form.body = leaveMessage;
-
-				if (leaveMessage.includes("{userNameTag}")) {
-					form.mentions = [{
-						id: leftParticipantFbId,
-						tag: userName
-					}];
-				}
-
-				if (threadData.data.leaveAttachment) {
-					const files = threadData.data.leaveAttachment;
-					const attachments = files.reduce((acc, file) => {
-						acc.push(drive.getFile(file, "stream"));
-						return acc;
-					}, []);
-					form.attachment = (await Promise.allSettled(attachments))
-						.filter(({ status }) => status == "fulfilled")
-						.map(({ value }) => value);
-				}
-				message.send(form);
-			};
-	}
-};
+module.exports.config = {
+‎  name: "leave",
+‎  eventType: ["log:unsubscribe"],
+‎  version: "1.0.0",
+‎  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+‎  description: "Thông báo bot hoặc người rời khỏi nhóm",
+‎  dependencies: {
+‎    "fs-extra": "",
+‎    "path": ""
+‎  }
+‎};
+‎
+‎module.exports.run = async function({ api, event, Users, Threads }) {
+‎  if (event.logMessageData.leftParticipantFbId == api.getCurrentUserID()) return;
+‎
+‎  const { createReadStream, existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+‎  const { join } = global.nodemodule["path"];
+‎  const { threadID } = event;
+‎
+‎  const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
+‎  const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
+‎
+‎  const type = (event.author == event.logMessageData.leftParticipantFbId)
+‎    ? " তোর সাহস কম না  গ্রুপের এডমিনের পারমিশন ছাড়া তুই লিভ  নিস😡😠🤬 \n✦─────꯭─⃝‌‌SUMON BOT────✦"
+‎    : "সম্মানিত মেম্বার,
+‎
+‎🫵🫵🫵
+‎
+‎💔 আসলে আপনাকে কিক দিতে খুব কষ্ট হচ্ছে, কিন্তু কিছু করার নেই... 😔
+‎আপনি যখন সময় পাবেন, ফ্রী হবেন, যখন মনে হবে আপনি সময় দিতে পারবেন, তখন আমাদের এডমিন পারসনকে নক দিয়ে গ্রুপে এড হবেন।😊\⚠️ নোট:
+‎- আপনার অবদান আমাদের কাছে অনেক গুরুত্বপূর্ণ ছিল 🙏
+‎- আশা করি ভবিষ্যতে আবার একসাথে দেখা হবে 🌈
+‎- গ্রুপের নিয়ম-কানুন মেনে চলার জন্য ধন্যবাদ 👍
+‎- আপনার জন্য সবসময় ভালোবাসা রইলো ❤️
+‎
+‎আশা করি বুঝতে পেরেছেন 😊
+‎
+‎আপনার অপেক্ষায় রইলাম... ⌛
+‎
+‎ধন্যবাদ,😘🥰\n✦─────꯭─⃝‌‌SUMON BOT────✦";
+‎
+‎  const path = join(__dirname, "Shahadat", "leaveGif");
+‎  const gifPath = join(path, `leave1.gif`);
+‎
+‎  if (!existsSync(path)) mkdirSync(path, { recursive: true });
+‎
+‎  let msg = (typeof data.customLeave == "undefined")
+‎    ? "ইস {name} {type} "
+‎    : data.customLeave;
+‎
+‎  msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
+‎
+‎  const formPush = existsSync(gifPath)
+‎    ? { body: msg, attachment: createReadStream(gifPath) }
+‎    : { body: msg };
+‎
+‎  return api.sendMessage(formPush, threadID);
+‎};
+‎
